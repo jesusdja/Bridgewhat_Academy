e5b0e42635cb669ff8035889c003b845efce77bf
jesusdja <jesus.cortez.1991@gmail.com>
2022-07-11 11:30:20 -0400

    demo page

--- lib/config/academy_colors.dart
+++ lib/config/academy_colors.dart
@@ -12,4 +12,13 @@ class AcademyColors {
 
   static const Color colors_C4C4C4 = Color(0xFFC4C4C4);
 
+  static const Color colors_E8E8E8 = Color(0xF0E8E8E8);
+
+  static const Color colors_95C4E9 = Color(0xF095C4E9);
+
+  static const Color colors_85939D = Color(0xF085939D);
+
+  static const Color colors_958E6F = Color(0xF0958E6F);
+
+  static const Color colors_B2BEC6 = Color(0xF0B2BEC6);
 }
--- lib/ui/menu/demo/demo_page.dart
+++ lib/ui/menu/demo/demo_page.dart
@@ -1,3 +1,5 @@
+import 'package:academybw/config/academy_colors.dart';
+import 'package:academybw/config/academy_style.dart';
 import 'package:academybw/main.dart';
 import 'package:academybw/widgets_shared/widgets_shared.dart';
 import 'package:flutter/material.dart';
@@ -22,8 +24,101 @@ class _DemoPageState extends State<DemoPage> {
               margin: EdgeInsets.symmetric(horizontal: sizeW * 0.06),
               child: bannerTitle(type: 3),
             ),
+            Container(
+              width: sizeW,
+              margin: EdgeInsets.symmetric(horizontal: sizeW * 0.06),
+              child: Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      card(type: 0),
+                      card(type: 1),
+                      card(type: 2),
+                      card(type: 3),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget card({required int type}){
+
+    String title = 'CRYSTAL';
+    List<String> listTitle = ['Registration','Reviews','Sampling Marketing','Full content consumption',];
+    Color color = AcademyColors.colors_95C4E9;
+    if(type == 1){
+      title = 'SILVER';
+      color = AcademyColors.colors_85939D;
+      listTitle = ['All Crystal features +','Scoring Board','Protocols','Silver content package',];
+    }
+    if(type == 2){
+      title = 'GOLD';
+      color = AcademyColors.colors_958E6F;
+      listTitle = ['All Silver features +','“Need & Leeds”','Campaigns','Gold content package',];
+    }
+    if(type == 3){
+      title = 'PLATINUM';
+      color = AcademyColors.colors_B2BEC6;
+      listTitle = ['All Gold features +','Advisory Services','Platinum content package',];
+    }
+
+    List<Widget> listW = [];
+    for (var element in listTitle) {
+      listW.add(
+        Container(
+          width: sizeW,
+          margin: EdgeInsets.only(bottom: sizeH * 0.01),
+          child: Row(
+            children: [
+              Icon(Icons.circle,color: AcademyColors.primary,size: sizeH * 0.01,),
+              SizedBox(width: sizeW * 0.02),
+              Expanded(
+                child: Text(element,style: AcademyStyles().styleLato(size: 12,color: Colors.black,),),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Container(
+      width: sizeW,
+      margin: EdgeInsets.only(top: sizeH * 0.02),
+      height: sizeH * 0.16,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        color: AcademyColors.colors_E8E8E8,
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: sizeW * 0.3,
+            height: sizeH * 0.05,
+            margin: EdgeInsets.symmetric(horizontal: sizeW * 0.05),
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(5)),
+              color: color,
+            ),
+            child: Center(
+              child: Text(title,style: AcademyStyles().stylePoppins(
+                size: sizeH * 0.02,
+                color: Colors.white,
+                fontWeight: FontWeight.bold
+              )),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: listW,
+            ),
+          )
+        ],
       ),
     );
   }
