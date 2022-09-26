c4ef8dfd9c4c5ac78e2995c737147e361c1dbd4d
jesusdja <jesus.cortez.1991@gmail.com>
2022-09-26 11:40:17 -0400

    webView

--- lib/ui/menu/demo/demo_page_2.dart
+++ lib/ui/menu/demo/demo_page_2.dart
@@ -1,8 +1,8 @@
 import 'package:academybw/config/academy_colors.dart';
 import 'package:academybw/main.dart';
-import 'package:academybw/widgets_shared/circular_progress_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:webview_flutter/platform_interface.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class DemoPage2 extends StatefulWidget {
   const DemoPage2({Key? key}) : super(key: key);
@@ -13,7 +13,6 @@ class DemoPage2 extends StatefulWidget {
 
 class _DemoPage2State extends State<DemoPage2> {
 
-  late InAppWebViewController webView;
   bool loadData = true;
   double progressInt = 0.0;
 
@@ -40,7 +39,7 @@ class _DemoPage2State extends State<DemoPage2> {
                   if(loadData)...[
                     Center(
                       child: Container(
-                      alignment: Alignment.topCenter,
+                          alignment: Alignment.topCenter,
                           margin: EdgeInsets.all(20),
                           child: LinearProgressIndicator(
                             value: progressInt,
@@ -51,17 +50,11 @@ class _DemoPage2State extends State<DemoPage2> {
                       ),
                     )
                   ],
-                  InAppWebView(
-                    initialUrlRequest: URLRequest(url: Uri.parse("https://bridgewhat.ole.agency/demo/")),
-                    initialOptions: InAppWebViewGroupOptions(
-                      crossPlatform: InAppWebViewOptions(
-                        javaScriptEnabled: true,
-                      ),
-                    ),
-                    onWebViewCreated: (InAppWebViewController controller) {
-                      webView = controller;
-                    },
-                    onProgressChanged: (_controller, progress) async {
+                  WebView(
+                    initialUrl: 'https://bridgewhat.ole.agency/demo/',
+                    javascriptMode: JavascriptMode.unrestricted,
+                    backgroundColor: Colors.transparent,
+                    onProgress: (progress) async {
                       progressInt = double.parse(progress.toString())  / 100;
                       print('progressInt : $progressInt');
                       if(progress == 1.0){
@@ -69,9 +62,30 @@ class _DemoPage2State extends State<DemoPage2> {
                       }
                       setState(() {});
                     },
-                    onLoadStart: (_controller, url){},
-                    onLoadStop: (_controller, url){},
                   ),
+
+
+                  // InAppWebView(
+                  //   initialUrlRequest: URLRequest(url: Uri.parse("https://bridgewhat.ole.agency/demo/")),
+                  //   initialOptions: InAppWebViewGroupOptions(
+                  //     crossPlatform: InAppWebViewOptions(
+                  //       javaScriptEnabled: true,
+                  //     ),
+                  //   ),
+                  //   onWebViewCreated: (InAppWebViewController controller) {
+                  //     webView = controller;
+                  //   },
+                  //   onProgressChanged: (_controller, progress) async {
+                  //     progressInt = double.parse(progress.toString())  / 100;
+                  //     print('progressInt : $progressInt');
+                  //     if(progress == 1.0){
+                  //       loadData = false;
+                  //     }
+                  //     setState(() {});
+                  //   },
+                  //   onLoadStart: (_controller, url){},
+                  //   onLoadStop: (_controller, url){},
+                  // ),
                 ],
               ),
             ),
@@ -116,3 +130,123 @@ class _DemoPage2State extends State<DemoPage2> {
     );
   }
 }
+
+
+// import 'package:academybw/config/academy_colors.dart';
+// import 'package:academybw/main.dart';
+// import 'package:academybw/widgets_shared/circular_progress_colors.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+//
+// class DemoPage2 extends StatefulWidget {
+//   const DemoPage2({Key? key}) : super(key: key);
+//
+//   @override
+//   State<DemoPage2> createState() => _DemoPage2State();
+// }
+//
+// class _DemoPage2State extends State<DemoPage2> {
+//
+//   late InAppWebViewController webView;
+//   bool loadData = true;
+//   double progressInt = 0.0;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+//
+//   @override
+//   void dispose() {
+//     super.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SizedBox(
+//         child: Column(
+//           children: <Widget>[
+//             headerContainer(),
+//             Expanded(
+//               child: Stack(
+//                 children: [
+//                   if(loadData)...[
+//                     Center(
+//                       child: Container(
+//                       alignment: Alignment.topCenter,
+//                           margin: EdgeInsets.all(20),
+//                           child: LinearProgressIndicator(
+//                             value: progressInt,
+//                             backgroundColor: Colors.grey.withOpacity(0.5),
+//                             color: AcademyColors.primary,
+//                             minHeight: 10,
+//                           )
+//                       ),
+//                     )
+//                   ],
+//                   InAppWebView(
+//                     initialUrlRequest: URLRequest(url: Uri.parse("https://bridgewhat.ole.agency/demo/")),
+//                     initialOptions: InAppWebViewGroupOptions(
+//                       crossPlatform: InAppWebViewOptions(
+//                         javaScriptEnabled: true,
+//                       ),
+//                     ),
+//                     onWebViewCreated: (InAppWebViewController controller) {
+//                       webView = controller;
+//                     },
+//                     onProgressChanged: (_controller, progress) async {
+//                       progressInt = double.parse(progress.toString())  / 100;
+//                       print('progressInt : $progressInt');
+//                       if(progress == 1.0){
+//                         loadData = false;
+//                       }
+//                       setState(() {});
+//                     },
+//                     onLoadStart: (_controller, url){},
+//                     onLoadStop: (_controller, url){},
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//
+//   Widget headerContainer(){
+//     return Container(
+//       width: sizeW,
+//       margin: EdgeInsets.only(left: sizeW * 0.06, right: sizeW * 0.03,top: 30),
+//       child: Row(
+//         children: [
+//           SizedBox(
+//             width: sizeW * 0.07,
+//             child: InkWell(
+//               child: Icon(Icons.arrow_back_ios,size: sizeH * 0.035,color: AcademyColors.primary),
+//               onTap: (){
+//                 Navigator.of(context).pop();
+//               },
+//             ),
+//           ),
+//           iconApp(),
+//           Expanded(child: Container()),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   Widget iconApp(){
+//     return Container(
+//       height: sizeH * 0.1,
+//       width: sizeW * 0.25,
+//       decoration: BoxDecoration(
+//         image: DecorationImage(
+//             image: Image.asset('assets/image/logo_colores_fondo_transparente.png').image,
+//             fit: BoxFit.fitWidth
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/ui/menu/videos/widgets/sub_videos_view.dart
+++ lib/ui/menu/videos/widgets/sub_videos_view.dart
@@ -2,8 +2,9 @@ import 'package:academybw/config/academy_colors.dart';
 import 'package:academybw/main.dart';
 import 'package:academybw/ui/menu/videos/provider/videos_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:provider/provider.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class SubVideosView extends StatefulWidget {
   const SubVideosView({
@@ -23,7 +24,7 @@ class _SubVideosViewState extends State<SubVideosView> {
   // late VideoPlayerController _controller;
 
   // late FlickManager flickManager;
-  late InAppWebViewController webView;
+  // late InAppWebViewController webView;
 
   @override
   void initState() {
@@ -40,7 +41,7 @@ class _SubVideosViewState extends State<SubVideosView> {
     // );
 
     Future.delayed(const Duration(milliseconds: 500)).then((value) =>
-    videosProvider.getVideos());
+        videosProvider.getVideos());
   }
 
   @override
@@ -58,19 +59,35 @@ class _SubVideosViewState extends State<SubVideosView> {
       body: Column(children: <Widget>[
         headerContainer(),
         Expanded(
-            child: InAppWebView(
-              initialUrlRequest: URLRequest(url: Uri.parse(video)),
-              initialOptions: InAppWebViewGroupOptions(
-                crossPlatform: InAppWebViewOptions(
-                  javaScriptEnabled: true,
-                ),
-              ),
-              onWebViewCreated: (InAppWebViewController controller) {
-                webView = controller;
-              },
-              onLoadStart: (_controller, url){},
-              onLoadStop: (_controller, url){},
-            ),
+          child: WebView(
+            initialUrl: video,
+            javascriptMode: JavascriptMode.unrestricted,
+            backgroundColor: Colors.transparent,
+            // onProgress: (progress) async {
+            //   progressInt = double.parse(progress.toString())  / 100;
+            //   print('progressInt : $progressInt');
+            //   if(progress == 1.0){
+            //     loadData = false;
+            //   }
+            //   setState(() {});
+            // },
+          ),
+          // child: WebView(
+          //   initialUrl: video,
+          // ),
+          // child: InAppWebView(
+          //   initialUrlRequest: URLRequest(url: Uri.parse(video)),
+          //   initialOptions: InAppWebViewGroupOptions(
+          //     crossPlatform: InAppWebViewOptions(
+          //       javaScriptEnabled: true,
+          //     ),
+          //   ),
+          //   onWebViewCreated: (InAppWebViewController controller) {
+          //     webView = controller;
+          //   },
+          //   onLoadStart: (_controller, url){},
+          //   onLoadStop: (_controller, url){},
+          // ),
         )
       ],
       ),
@@ -167,3 +184,174 @@ class _SubVideosViewState extends State<SubVideosView> {
     );
   }
 }
+
+
+// import 'package:academybw/config/academy_colors.dart';
+// import 'package:academybw/main.dart';
+// import 'package:academybw/ui/menu/videos/provider/videos_provider.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+// import 'package:provider/provider.dart';
+//
+// class SubVideosView extends StatefulWidget {
+//   const SubVideosView({
+//     Key? key, required this.video
+//   }) : super(key: key);
+//
+//   final String video;
+//
+//   @override
+//   State<SubVideosView> createState() => _SubVideosViewState();
+// }
+//
+// class _SubVideosViewState extends State<SubVideosView> {
+//
+//   String video = '';
+//   late VideosProvider videosProvider;
+//   // late VideoPlayerController _controller;
+//
+//   // late FlickManager flickManager;
+//   late InAppWebViewController webView;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     video = widget.video;
+//     // _controller = VideoPlayerController.network(video)
+//     // ..initialize().then((_) {
+//     //   setState(() {});
+//     // });
+//     //
+//     // flickManager = FlickManager(
+//     //   videoPlayerController:
+//     //   VideoPlayerController.network(video),
+//     // );
+//
+//     Future.delayed(const Duration(milliseconds: 500)).then((value) =>
+//     videosProvider.getVideos());
+//   }
+//
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     // _controller.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     videosProvider = Provider.of<VideosProvider>(context);
+//
+//     return Scaffold(
+//       body: Column(children: <Widget>[
+//         headerContainer(),
+//         Expanded(
+//             child: InAppWebView(
+//               initialUrlRequest: URLRequest(url: Uri.parse(video)),
+//               initialOptions: InAppWebViewGroupOptions(
+//                 crossPlatform: InAppWebViewOptions(
+//                   javaScriptEnabled: true,
+//                 ),
+//               ),
+//               onWebViewCreated: (InAppWebViewController controller) {
+//                 webView = controller;
+//               },
+//               onLoadStart: (_controller, url){},
+//               onLoadStop: (_controller, url){},
+//             ),
+//         )
+//       ],
+//       ),
+//     );
+//   }
+//
+//   // @override
+//   // Widget build(BuildContext context) {
+//   //
+//   //   videosProvider = Provider.of<VideosProvider>(context);
+//   //
+//   //   return Scaffold(
+//   //     body: SizedBox(
+//   //       width: sizeW,
+//   //       child: Column(
+//   //         children: [
+//   //           headerContainer(),
+//   //           Expanded(
+//   //             child: Column(
+//   //               crossAxisAlignment: CrossAxisAlignment.center,
+//   //               mainAxisAlignment: MainAxisAlignment.center,
+//   //               children: [
+//   //                 cardVideo(),
+//   //               ],
+//   //             ),
+//   //           )
+//   //         ],
+//   //       ),
+//   //     ),
+//   //   );
+//   // }
+//
+//   // Widget cardVideo(){
+//   //   return Container(
+//   //     width: sizeW,
+//   //     height: sizeH * 0.3,
+//   //     margin: EdgeInsets.only(bottom: sizeH * 0.02),
+//   //     decoration: BoxDecoration(
+//   //       color: Colors.grey[100],
+//   //       borderRadius: const BorderRadius.all(Radius.circular(10)),
+//   //     ),
+//   //     child: Stack(
+//   //       children: [
+//   //         InkWell(
+//   //           child: SizedBox(
+//   //             width: sizeW,
+//   //             height: sizeH * 0.3,
+//   //             child: Center(
+//   //               child: _controller.value.isInitialized
+//   //                   ? FlickVideoPlayer(
+//   //                   flickManager: flickManager
+//   //               ) :
+//   //               circularProgressColors(widthContainer1: sizeW, widthContainer2: sizeW * 0.06,colorCircular: AcademyColors.primary),
+//   //             ),
+//   //           ),
+//   //         ),
+//   //       ],
+//   //     ),
+//   //   );
+//   // }
+//
+//   Widget headerContainer(){
+//     return Container(
+//       width: sizeW,
+//       margin: EdgeInsets.only(left: sizeW * 0.06, right: sizeW * 0.03,top: 30),
+//       child: Row(
+//         children: [
+//           SizedBox(
+//             width: sizeW * 0.07,
+//             child: InkWell(
+//               child: Icon(Icons.arrow_back_ios,size: sizeH * 0.035,color: AcademyColors.primary),
+//               onTap: (){
+//                 Navigator.of(context).pop();
+//               },
+//             ),
+//           ),
+//           iconApp(),
+//           Expanded(child: Container()),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   Widget iconApp(){
+//     return Container(
+//       height: sizeH * 0.1,
+//       width: sizeW * 0.25,
+//       decoration: BoxDecoration(
+//         image: DecorationImage(
+//             image: Image.asset('assets/image/logo_colores_fondo_transparente.png').image,
+//             fit: BoxFit.fitWidth
+//         ),
+//       ),
+//     );
+//   }
+// }
